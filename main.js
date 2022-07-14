ef8c69b895dc6ccce97eafee0fa1eb7d0fa533de
oleGgggga <vasileuski23@gmail.com>
2022-07-14 19:04:12 +0300

    done part of hw3

---
+++ main.js
@@ -0,0 +1,28 @@
+for(let i=1;i<=50;i++){
+    console.log(i);
+}
+/////////////////
+for(let i=35;i>=8 && i<=35;i--){
+    console.log(i);
+}
+////////////////////
+let y=89;
+while(y>=11){
+    document.write(`${y}<br>`);
+    y--;
+}
+/////////////////////
+sum=0;
+for(let i=0; i<=100; i++){
+    sum+=i;
+    
+}
+console.log(sum);
+/////////////////
+for(let i=56;i>=8 && i<=56;i--){
+    if (i%2===0){
+    console.log(i);
+    }
+}
+////////////////
+
